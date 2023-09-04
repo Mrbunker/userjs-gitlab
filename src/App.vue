e9a9b9d52753f5ef952422da60c413fc77456c66
mrbunker <mrbunker@qq.com>
2023-09-04 23:12:30 +0800

    chore: rename the script

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import { ref } from "vue";
 
 import HelperMenu from "@/components/HelperMenu.vue";
 
-const svg =
-  "http://gitlab.shxgroup.net/assets/icons-09fdf2c02921bad2ec7257465016a755f359ab7b598e5fe42c22381fe1a25045.svg#angle-down";
 const menuShow = ref(false);
 const handleMenu = (show: boolean) => {
   menuShow.value = show;
@@ -16,10 +14,7 @@ const handleMenu = (show: boolean) => {
     class="header-user-dropdown-toggle tw-cursor-pointer"
     @mouseenter="handleMenu(true)"
   >
-    helper
-    <svg class="caret-down">
-      <use :xlink:href="svg" />
-    </svg>
+    😎🤞
   </a>
 
   <HelperMenu :open="menuShow" :handle-menu="handleMenu" />
